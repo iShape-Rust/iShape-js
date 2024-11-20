6e33d3f182e43282f27fca11fce8d0cc75962a0f
Nail Sharipov <nailxsharipov@gmail.com>
2024-11-20 17:40:00 +0300

    new 1.1.2 ver

--- pkg/ishape_wasm_bg.wasm.d.ts
+++ pkg/ishape_wasm_bg.wasm.d.ts
@@ -7,4 +7,7 @@ export function overlay_overlay(a: number, b: number, c: number): number;
 export function overlay_separate_vectors(a: number, b: number): number;
 export function __wbindgen_malloc(a: number, b: number): number;
 export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
 export function __wbindgen_exn_store(a: number): void;
+export function __externref_table_alloc(): number;
+export function __wbindgen_start(): void;
