5af7ab77c4beae695e8656e579d6b112db37340f
Nail Sharipov <nailxsharipov@gmail.com>
2025-04-24 14:01:27 +0300

    fix path

--- docs-gen/src/js/overlay_editor/web_app.js
+++ docs-gen/src/js/overlay_editor/web_app.js
@@ -4,10 +4,10 @@ async function run() {
     await init();
     console.log('wasm module loaded');
 
-    const booleanData = await fetch('./tests/boolean_tests.json').then((res) => res.text());
-    const stringData = await fetch('./tests/string_tests.json').then((res) => res.text());
-    const strokeData = await fetch('./tests/stroke_tests.json').then((res) => res.text());
-    const outlineData = await fetch('./tests/outline_tests.json').then((res) => res.text());
+    const booleanData = await fetch('./../js/overlay_editor/tests/boolean_tests.json').then((res) => res.text());
+    const stringData = await fetch('./../js/overlay_editor/tests/string_tests.json').then((res) => res.text());
+    const strokeData = await fetch('./../js/overlay_editor/tests/stroke_tests.json').then((res) => res.text());
+    const outlineData = await fetch('./../js/overlay_editor/tests/outline_tests.json').then((res) => res.text());
     console.log('json files loaded');
 
     const app = new WebApp();
