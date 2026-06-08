6a4b716e6ea21c38fa705689268e095e6e6e1ef1
Nail Sharipov <nailxsharipov@gmail.com>
2026-06-08 13:14:36 +0300

    fix performance result

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-6cca56a9.js',
+                path_to_root + 'searchindex-44691e32.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
